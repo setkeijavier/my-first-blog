1d5adb5f80527545b1c5dd3361b08e463563d688
Your Name <you@example.com>
2015-06-25 15:45:46 -0500

    Ahora se puede ver un post particular

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.exclude(published_date__isnull=True).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+	
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
